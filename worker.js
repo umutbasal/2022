50bd8e715e889f29664527fcc13764249751c8e7
umutbasal <umut.bsl@yandex.com>
2021-12-15 21:46:53 +0300

    log req body and make key unique

--- worker.js
+++ worker.js
@@ -14,7 +14,19 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request, event) {
-	await LOGS.put(request.headers.get("cf-connecting-ip"), JSON.stringify({ headers: [...request.headers], url: request.url, method: request.method, date: Date.now() }))
+    let body;
+	let reqclone = request.clone()
+    if (request.body){
+        let reader = reqclone.body.getReader();
+        while (true) {
+            const {done, value} = await reader.read();
+            if (done) {
+                break;
+            }
+            body = btoa(new TextDecoder().decode(value));
+        }
+    }
+    await LOGS.put(request.headers.get("cf-connecting-ip")+"-"+Date.now(), JSON.stringify({ headers: [...request.headers], url: request.url, method: request.method, body: body, date: Date.now() }))
 
 	if (request.headers.get('user-agent')?.includes("curl")) {
 		return fetch(request);
