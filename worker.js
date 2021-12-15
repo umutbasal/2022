1e45795cd1c6cde4cea56a3b96b53a66c6fc034c
umutbasal <umut.bsl@yandex.com>
2021-12-15 19:05:39 +0300

    log reqs

--- worker.js
+++ worker.js
@@ -1,18 +1,24 @@
-// deployed on https://2022.umut.ninja
 const ORIGIN = 'newyeartree.herokuapp.com';
 
-addEventListener('fetch', event => {    
-    var req = event.request;
-    
-    const modifiedRequest = new Request("https://"+ORIGIN, {
-        body: req.body,
-        headers: req.headers,
-        method: req.method,
-        redirect: req.redirect
-    })
-    if (req.headers.get('user-agent')?.includes("curl")){
-        event.respondWith(fetch(modifiedRequest));
-    } else {
-        event.respondWith(Response.redirect("https://twitter.com/umutbasalt", 301))
-    }
+addEventListener('fetch', event => {
+	var req = event.request;
+
+	const modifiedRequest = new Request("https://" + ORIGIN, {
+		body: req.body,
+		headers: req.headers,
+		method: req.method,
+		redirect: req.redirect
+	});
+
+	event.respondWith(handleRequest(modifiedRequest, event));
 })
+
+async function handleRequest(request, event) {
+	await LOGS.put(request.headers.get("cf-connecting-ip"), JSON.stringify({ headers: [...request.headers], url: request.url, method: request.method, date: Date.now() }))
+
+	if (request.headers.get('user-agent')?.includes("curl")) {
+		return fetch(request);
+	} else {
+		return Response.redirect("https://twitter.com/umutbasalt", 301);
+	}
+}
